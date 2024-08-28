d3b5169346268a889ab175bbd66b0987df4700a2
gbmello <gabriel_meello@hotmail.com>
2024-08-28 09:30:27 -0300

    fifth commit - implements dto

---
+++ src/main/java/com/gabrielmello/apiEvents/domain/event/EventRequestDTO.java
@@ -0,0 +1,8 @@
+package com.gabrielmello.apiEvents.domain.event;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+public record EventRequestDTO(String title, String description, Date date, String city, String State, Boolean remote, String eventUrl, MultipartFile image) {
+}
