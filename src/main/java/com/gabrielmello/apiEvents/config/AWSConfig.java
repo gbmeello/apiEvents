137aaae3553c4e9e7b4fa6ca9e1f12199a61270f
gbmello <gabriel_meello@hotmail.com>
2024-08-28 09:55:48 -0300

    Seventh commit - implements awsconfig and service

---
+++ src/main/java/com/gabrielmello/apiEvents/config/AWSConfig.java
@@ -0,0 +1,22 @@
+package com.gabrielmello.apiEvents.config;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 createS3Instance(){
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(awsRegion)
+                .build();
+    }
+
+}
--- src/main/java/com/gabrielmello/apiEvents/service/EventService.java
+++ src/main/java/com/gabrielmello/apiEvents/service/EventService.java
@@ -1,4 +1,34 @@
 package com.gabrielmello.apiEvents.service;
 
+import com.gabrielmello.apiEvents.domain.event.Event;
+import com.gabrielmello.apiEvents.domain.event.EventRequestDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+@Service
 public class EventService {
+
+    public Event createEvent(EventRequestDTO data){
+
+        String imgUrl = null;
+
+        if(data.image() != null){
+           imgUrl =  this.uploadImg(data.image());
+        }
+
+        Event newEvent = new Event();
+        newEvent.setTitle(data.title());
+        newEvent.setDescription(data.description());
+        newEvent.setEventUrl(data.eventUrl());
+        newEvent.setDate(new Date(data.date()));
+        newEvent.setImgUrl(imgUrl);
+        return  newEvent;
+    }
+
+    private String uploadImg(MultipartFile image) {
+        return "";
+    }
+
 }
